fe5b2e2aed2f428109d50ea62bc7628e30cafeaa
shevchuk11 <v.shevchuk.1105@gmail.com>
2019-03-26 23:45:45 +0200

    11
    js

---
+++ even.js
@@ -0,0 +1,10 @@
+document.getElementById("but").addEventListener("click", pressButton);
+function pressButton(){       //
+	console.log(document.getElementById('inp').value);
+	console.log(document.getElementById('inp1').value);
+
+	// var a = (document.getElementById('pr'));
+	// a.innerHTML=document.getElementById('inp').value;
+
+		// body...
+	}
